10aadf1063dccdc385c5f40ea826f0df1c148070
C0deNe0 <access.naveenhiremath@gmail.com>
2026-02-26 23:51:51 +0530

    fix

--- app/components/NaveenHiremathMark.tsx
+++ app/components/NaveenHiremathMark.tsx
@@ -15,5 +15,5 @@ export function NaveenHiremathMark(props: React.ComponentProps<"svg">) {
 }
 
 export function getMarkSVG(color: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"/></svg>`
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M 70.2 70 L 70.2 0 L 104.2 0 L 104.2 31.3 L 126.3 31.3 L 126.3 0 L 160.3 0 L 160.3 70 L 126.3 70 L 126.3 38 L 104.2 38 L 104.2 70 L 70.2 70 Z M 0 70 L 0 0 L 29.4 0 L 58.6 35 L 58.6 0 L 64.6 0 L 64.6 70 L 41.1 70 L 6.1 23.4 L 6.1 70 L 0 70 Z""/></svg>`
 }
--- app/components/ProfileCover.tsx
+++ app/components/ProfileCover.tsx
@@ -9,13 +9,13 @@ export function ProfileCover() {
       <div
         className={cn(
             "h-80",
-          "aspect-2/1 select-none sm:aspect-3/1",
+          "aspect-2/1 select-none sm:aspect-3/1 border-2 border-gray-300  dark:border-gray-950",
           "flex items-center justify-center text-black dark:text-white",
           "screen-line-before screen-line-after before:-top-px after:-bottom-px",
-          "bg-black/0.75 bg-[radial-gradient(var(--pattern-foreground)_1px,transparent_0)] bg-size-[10px_10px] bg-center [--pattern-foreground:var(--color-zinc-950)]/5 dark:bg-white/0.75 dark:[--pattern-foreground:var(--color-white)]/5"
+          "bg-black/0.75 bg-[radial-gradient(var(--pattern-foreground)_1px,transparent_0)] bg-size-[10px_10px] bg-center [--pattern-foreground:var(--color-zinc-950)]/5 dark:bg-white/0.75 dark:[--pattern-foreground:var(--color-white)]/5 mb-7"
         )}
       >
-        <Magnet magnetStrength={6}>
+        <Magnet magnetStrength={3}>
           <NaveenHiremathMark
             id="js-cover-mark"
             className="h-14 w-28 sm:h-16 sm:w-32"
